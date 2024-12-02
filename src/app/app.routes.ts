a0cadad8a873d4f64bde075be60230596d7b09a0
Tania Rosales <75716621+tan-cloud95@users.noreply.github.com>
2024-12-02 14:56:28 -0600

    nueva ruta component userProfile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { ProgressComponent } from './components/progress/progress.component';
 import { StatisticsComponent } from './components/statistics/statistics.component';
 import { FrontpageComponent } from './components/frontpage/frontpage.component';
 import { LoginGuardian } from './components/login/login-guardian';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 export const routes: Routes = [
   {path: "", component: HomeComponent, canActivate: [LoginGuardian]},
@@ -16,6 +17,7 @@ export const routes: Routes = [
   {path: "listHabits", component: HabitListComponent, canActivate: [LoginGuardian]},
   {path: "progress", component: ProgressComponent, canActivate: [LoginGuardian]},
   {path: "statistics", component: StatisticsComponent, canActivate: [LoginGuardian]},
+  {path: "userProfile", component: UserProfileComponent, canActivate: [LoginGuardian]},
   {path: "frontpage", component: FrontpageComponent},
   {path: "login", component: LoginComponent},
   {path: "register", component: RegisterComponent},
