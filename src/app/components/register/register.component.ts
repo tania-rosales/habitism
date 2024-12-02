f1fc6061dd49fa575d870593a03b7383831c6429
Tania Rosales <75716621+tan-cloud95@users.noreply.github.com>
2024-12-01 21:19:59 -0600

    ci(register): registro completado

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { LoginService } from '../../services/login.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-register',
+  selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterModule],
+  providers: [],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  constructor (private loginService: LoginService){}
+
+  email: string = '';
+  password: string = '';
+  errorMessage: string | null = null;
+
+  onRegister () {
+    this.loginService.registro(this.email, this.password)
+    .then(() => {
+        this.errorMessage = null; // Limpiamos el mensaje en caso de éxito
+      })
+      .catch(error => {
+        this.errorMessage = `Error inesperado: ${error.message}`;
+      });
+  }
 
 }
